95f2ac24250f7d173d28df4996b081a51546f2ba
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-18 16:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener('click', function() {
+  // Your code to handle the submit event will go here
+});
+const player1 = document.getElementById('player-1').value;
+const player2 = document.getElementById('player-2').value;
